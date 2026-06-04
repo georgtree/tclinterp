d1a54e7670eacbb3e1d3fda2807db7a5f12bed01
Георгий Яшин <georgtree@gmail.com>
2026-06-04 17:03:46 +0100

    code style update in example, docs update

--- examples/linear_near_interpolation.tcl
+++ examples/linear_near_interpolation.tcl
@@ -39,33 +39,34 @@ set xydataInterpNear [lmap xi $xInterp yi $yInterpNear {list $xi $yi}]
 # exact data at interpolation points
 set xydataExact [lmap xi $xInterp yi $yExact {list $xi $yi}]
 # errors calculation
-set interpErrorLin [lmap xi $xInterp yiExact $yExact yiInterpLin $yInterpLin {list $xi [relError $yiExact $yiInterpLin]}]
+set interpErrorLin [lmap xi $xInterp yiExact $yExact yiInterpLin $yInterpLin\
+                            {list $xi [relError $yiExact $yiInterpLin]}]
 set interpErrorNear [lmap xi $xInterp yiExact $yExact yiInterpNear $yInterpNear\
                              {list $xi [relError $yiExact $yiInterpNear]}]
 
 # plot interpolated data and errors
 set chart [ticklecharts::chart new]
-$chart Xaxis -name "x" -minorTick {show "True"} -min 0 -max 1 -type "value" -splitLine {show "True"}
-$chart Yaxis -name "y" -minorTick {show "True"} -min 0 -max 2.5 -type "value" -splitLine {show "True"}
-$chart SetOptions -title {} -legend {top "0%" left "20%"} -tooltip {trigger "axis"} -animation "False"\
-        -toolbox {feature {dataZoom {yAxisIndex "none"}}}
-$chart Add "lineSeries" -data $xydata -showAllSymbol "nothing" -name "Initial data"
-$chart Add "lineSeries" -data $xydataInterpLin -showAllSymbol "nothing" -name "Linear interpolation"
-$chart Add "lineSeries" -data $xydataInterpNear -showAllSymbol "nothing" -name "Nearest interpolation"
-$chart Add "lineSeries" -data $xydataExact -showAllSymbol "nothing" -name "Exact data"
+$chart Xaxis -name x -minorTick {show True} -min 0 -max 1 -type value -splitLine {show True}
+$chart Yaxis -name y -minorTick {show True} -min 0 -max 2.5 -type value -splitLine {show True}
+$chart SetOptions -title {} -legend {top 0% left 20%} -tooltip {trigger axis} -animation False\
+        -toolbox {feature {dataZoom {yAxisIndex none}}}
+$chart Add lineSeries -data $xydata -showAllSymbol nothing -name {Initial data}
+$chart Add lineSeries -data $xydataInterpLin -showAllSymbol nothing -name {Linear interpolation}
+$chart Add lineSeries -data $xydataInterpNear -showAllSymbol nothing -name {Nearest interpolation}
+$chart Add lineSeries -data $xydataExact -showAllSymbol nothing -name {Exact data}
 
 set chartError [ticklecharts::chart new]
-$chartError Xaxis -name "x" -minorTick {show "True"} -min 0 -max 1 -type "value" -splitLine {show "True"}
-$chartError Yaxis -name "Rel. error, %" -minorTick {show "True"} -type "value" -splitLine {show "True"}
-$chartError SetOptions -title {} -legend {top "50%" left "30%"} -tooltip {} -animation "False"\
-        -toolbox {feature {dataZoom {yAxisIndex "none"}}}
-$chartError Add "lineSeries" -data $interpErrorLin -showAllSymbol "nothing" -name "Linear interpolation error"
-$chartError Add "lineSeries" -data $interpErrorNear -showAllSymbol "nothing" -name "Nearest interpolation error"
+$chartError Xaxis -name x -minorTick {show True} -min 0 -max 1 -type value -splitLine {show True}
+$chartError Yaxis -name {Rel. error, %} -minorTick {show True} -type value -splitLine {show True}
+$chartError SetOptions -title {} -legend {top 50% left 30%} -tooltip {} -animation False\
+        -toolbox {feature {dataZoom {yAxisIndex none}}}
+$chartError Add lineSeries -data $interpErrorLin -showAllSymbol nothing -name {Linear interpolation error}
+$chartError Add lineSeries -data $interpErrorNear -showAllSymbol nothing -name {Nearest interpolation error}
 
 # create multiplot
 set layout [ticklecharts::Gridlayout new]
-$layout Add $chart -bottom "55%" -height "40%" -width "80%"
-$layout Add $chartError -bottom "5%" -height "40%" -width "80%"
+$layout Add $chart -bottom 55% -height 40% -width 80%
+$layout Add $chartError -bottom 5% -height 40% -width 80%
 
 # save to file
 set fbasename [file rootname [file tail [info script]]]
